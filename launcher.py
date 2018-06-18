9c14039f7954e881c8d9e41c738c47c2337b09be
ron1196 <ron1196@gmail.com>
2018-06-19 01:40:49 +0300

    New Launcher

--- launcher.py
+++ launcher.py
@@ -2,16 +2,23 @@
 
 import sys
 import os
+import time
 import subprocess
 import argparse
 
 #Launcher for Jessie
 
 def parse_cli_args():
-    parser = argparse.ArgumentParser(description="Jessie Launcher - Pokemon Go Bot for Discord")
-    parser.add_argument("--start","-s",help="Starts Jessie",action="store_true")
-    parser.add_argument("--auto-restart","-r",help="Auto-Restarts Jessie in case of a crash.",action="store_true")
-    parser.add_argument("--debug","-d",help="Prevents output being sent to Discord DM, as restarting could occur often.",action="store_true")
+    parser = argparse.ArgumentParser(
+        description="Jessie Launcher - Pokemon Go Bot for Discord")
+    parser.add_argument(
+        "--auto-restart", "-r",
+        help="Auto-Restarts Jessie in case of a crash.", action="store_true")
+    parser.add_argument(
+        "--debug", "-d",
+        help=("Prevents output being sent to Discord DM, "
+              "as restarting could occur often."),
+        action="store_true")
     return parser.parse_args()
 
 def run_jessie(autorestart):
@@ -19,7 +26,9 @@ def run_jessie(autorestart):
     if interpreter is None:
         raise RuntimeError("Python could not be found")
 
-    cmd = [interpreter, "jessie", "launcher"]
+    cmd = [interpreter, "-m", "Jessie", "launcher"]
+
+    retries = 0
 
     while True:
         if args.debug:
@@ -34,6 +43,7 @@ def run_jessie(autorestart):
                 break
             elif code == 26:
                 #standard restart
+                retries = 0
                 print("")
                 print("Restarting Jessie")
                 print("")
@@ -41,9 +51,17 @@ def run_jessie(autorestart):
             else:
                 if not autorestart:
                     break
+                retries += 1
+                wait_time = min([retries^2, 60])
                 print("")
-                print("Restarting Jessie from crash")
+                print("Jessie experienced a crash.")
                 print("")
+                for i in range(wait_time, 0, -1):
+                    sys.stdout.write("\r")
+                    sys.stdout.write(
+                        "Restarting Jessie from crash in {:0d}".format(i))
+                    sys.stdout.flush()
+                    time.sleep()
 
     print("Jessie has closed. Exit code: {exit_code}".format(exit_code=code))
 
@@ -53,7 +71,5 @@ if __name__ == '__main__':
     abspath = os.path.abspath(__file__)
     dirname = os.path.dirname(abspath)
     os.chdir(dirname)
-
-    if args.start:
-        print("Launching Jessie...")
-        run_jessie(autorestart=args.auto_restart)
+    print("Launching Jessie...")
+    run_Meo(autorestart=args.auto_restart)
